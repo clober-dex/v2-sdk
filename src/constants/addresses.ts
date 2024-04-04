117de76980d80c5caf99d3cf56eef84caddfaf7f
graykode <nlkey2022@gmail.com>
2024-04-05 01:27:43 +0900

    fix: fix build failed

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -8,10 +8,6 @@ export const CONTRACT_ADDRESSES: {
     BookManager: `0x${string}`
   }
 } = {
-  [CHAIN_IDS.CLOBER_TESTNET]: {
-    Controller: getAddress('0x2489e8daffb7A4900074840C24e8096A25e2Eeb0'),
-    BookManager: getAddress('0x6411E5f824E1386d62ea20bFE5d815933F7E8491'),
-  },
   [CHAIN_IDS.ARBITRUM_SEPOLIA]: {
     Controller: getAddress('0x2489e8daffb7A4900074840C24e8096A25e2Eeb0'),
     BookManager: getAddress('0x6411E5f824E1386d62ea20bFE5d815933F7E8491'),
