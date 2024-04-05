0e07c771641538be180a696dff23af52d2758043
graykode <nlkey2022@gmail.com>
2024-04-05 10:56:49 +0900

    test: add limit order test

--- test/limit-order.test.ts
+++ test/limit-order.test.ts
@@ -1,6 +1,7 @@
 import { afterAll, expect, test } from 'vitest'
 import { limitOrder, signERC20Permit } from '@clober-dex/v2-sdk'
 import { mnemonicToAccount } from 'viem/accounts'
+import { formatUnits } from 'viem'
 
 import { toBookId } from '../src/utils/book-id'
 
@@ -10,7 +11,7 @@ import { FORK_BLOCK_NUMBER, FORK_URL, TEST_MNEMONIC } from './utils/constants'
 import { fetchTokenBalance } from './utils/currency'
 import { fetchDepth } from './utils/depth'
 
-const clients = createProxyClients([3, 4, 5, 6, 7])
+const clients = createProxyClients([3, 4, 5, 6, 7, 8, 9, 10])
 const account = mnemonicToAccount(TEST_MNEMONIC)
 
 afterAll(async () => {
@@ -288,4 +289,195 @@ test('make ask order with post only', async () => {
 
   expect(Number(beforeBalance - afterBalance)).greaterThan(Number(10n ** 16n))
   expect(Number(afterSize)).greaterThan(0)
+})
+
+test('limit bid order', async () => {
+  const bidBookId = toBookId(
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '0x0000000000000000000000000000000000000000',
+    1n,
+  )
+  const askBookId = toBookId(
+    '0x0000000000000000000000000000000000000000',
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    10n ** 12n,
+  )
+  const { walletClient, publicClient } = clients[6]
+  const signature = await signERC20Permit(
+    cloberTestChain.id,
+    account,
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '1000',
+    { rpcUrl: publicClient.transport.url! },
+  )
+  const transaction = await limitOrder(
+    cloberTestChain.id,
+    account.address,
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '0x0000000000000000000000000000000000000000',
+    '1000',
+    '4030.01',
+    { signature, rpcUrl: publicClient.transport.url! },
+  )
+
+  const [beforeUSDCBalance, beforeETHBalance] = await Promise.all([
+    fetchTokenBalance(
+      cloberTestChain.id,
+      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      account.address,
+      publicClient.transport.url!,
+    ),
+    publicClient.getBalance({
+      address: account.address,
+    }),
+  ])
+  const beforeBidSize = (
+    (
+      await fetchDepth(
+        cloberTestChain.id,
+        '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+        '0x0000000000000000000000000000000000000000',
+        bidBookId,
+        publicClient.transport.url!,
+      )
+    ).find(({ price }) => 4029 <= price && price <= 4030) ?? { amount: 0n }
+  ).amount
+  expect(Number(beforeBidSize)).toEqual(0)
+  const beforeAskDepths = await fetchDepth(
+    cloberTestChain.id,
+    '0x0000000000000000000000000000000000000000',
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    askBookId,
+    publicClient.transport.url!,
+  )
+
+  await walletClient.sendTransaction({ ...transaction!, account })
+
+  const [afterUSDCBalance, afterETHBalance] = await Promise.all([
+    fetchTokenBalance(
+      cloberTestChain.id,
+      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      account.address,
+      publicClient.transport.url!,
+    ),
+    publicClient.getBalance({
+      address: account.address,
+    }),
+  ])
+  const afterBidSize = (
+    await fetchDepth(
+      cloberTestChain.id,
+      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      '0x0000000000000000000000000000000000000000',
+      bidBookId,
+      publicClient.transport.url!,
+    )
+  ).find(({ price }) => 4029 <= price && price <= 4030)!.amount
+
+  expect(beforeUSDCBalance - afterUSDCBalance).toEqual(1000000000n)
+  expect(Number(formatUnits(afterETHBalance, 18))).greaterThan(
+    Number(formatUnits(beforeETHBalance, 18)),
+  )
+  expect(Number(afterBidSize)).greaterThan(0)
+  const afterAskDepths = await fetchDepth(
+    cloberTestChain.id,
+    '0x0000000000000000000000000000000000000000',
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    askBookId,
+    publicClient.transport.url!,
+  )
+  expect(beforeAskDepths.length).greaterThan(afterAskDepths.length)
+})
+
+test('limit ask order', async () => {
+  const bidBookId = toBookId(
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '0x0000000000000000000000000000000000000000',
+    1n,
+  )
+  const askBookId = toBookId(
+    '0x0000000000000000000000000000000000000000',
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    10n ** 12n,
+  )
+  const { walletClient, publicClient } = clients[7]
+  const transaction = await limitOrder(
+    cloberTestChain.id,
+    account.address,
+    '0x0000000000000000000000000000000000000000',
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '0.1',
+    '3999.50',
+    { rpcUrl: publicClient.transport.url! },
+  )
+
+  const [beforeUSDCBalance, beforeETHBalance] = await Promise.all([
+    fetchTokenBalance(
+      cloberTestChain.id,
+      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      account.address,
+      publicClient.transport.url!,
+    ),
+    publicClient.getBalance({
+      address: account.address,
+    }),
+  ])
+  const beforeAskSize = (
+    (
+      await fetchDepth(
+        cloberTestChain.id,
+        '0x0000000000000000000000000000000000000000',
+        '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+        askBookId,
+        publicClient.transport.url!,
+      )
+    ).find(({ price }) => 3999.5 <= price && price <= 3999.8) ?? { amount: 0n }
+  ).amount
+  expect(Number(beforeAskSize)).toEqual(0)
+  const beforeBidDepths = await fetchDepth(
+    cloberTestChain.id,
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '0x0000000000000000000000000000000000000000',
+    bidBookId,
+    publicClient.transport.url!,
+  )
+
+  await walletClient.sendTransaction({ ...transaction!, account })
+
+  const [afterUSDCBalance, afterETHBalance] = await Promise.all([
+    fetchTokenBalance(
+      cloberTestChain.id,
+      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      account.address,
+      publicClient.transport.url!,
+    ),
+    publicClient.getBalance({
+      address: account.address,
+    }),
+  ])
+  const afterAskSize = (
+    await fetchDepth(
+      cloberTestChain.id,
+      '0x0000000000000000000000000000000000000000',
+      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      askBookId,
+      publicClient.transport.url!,
+    )
+  ).find(({ price }) => 3999.5 <= price && price <= 3999.8)!.amount
+
+  expect(Number(beforeETHBalance - afterETHBalance)).greaterThan(
+    100000000000000000,
+  )
+  expect(Number(formatUnits(afterUSDCBalance, 6))).greaterThan(
+    Number(formatUnits(beforeUSDCBalance, 6)),
+  )
+  expect(Number(afterAskSize)).greaterThan(0)
+  const afterBidDepths = await fetchDepth(
+    cloberTestChain.id,
+    '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+    '0x0000000000000000000000000000000000000000',
+    bidBookId,
+    publicClient.transport.url!,
+  )
+  expect(beforeBidDepths.length).greaterThan(afterBidDepths.length)
 })
