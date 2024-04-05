4aa5c7e1e17f1cc3904f04b2eb98f15a111ed0fb
graykode <nlkey2022@gmail.com>
2024-04-05 10:43:04 +0900

    fix: build failed

--- src/constants/subgraph-url.ts
+++ src/constants/subgraph-url.ts
@@ -3,8 +3,6 @@ import { CHAIN_IDS } from './chain'
 export const SUBGRAPH_URL: {
   [chain in CHAIN_IDS]: string
 } = {
-  [CHAIN_IDS.CLOBER_TESTNET]:
-    'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-core-subgraph/api',
   [CHAIN_IDS.ARBITRUM_SEPOLIA]:
     'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-core-subgraph/api',
 }
