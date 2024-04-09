e84f7759703d7f2eca27f066b64470cdd9e513e1
graykode <nlkey2022@gmail.com>
2024-04-09 13:35:26 +0900

    feat: math logic

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -10,31 +10,108 @@ export const divide = (x: bigint, y: bigint, roundUp: boolean): bigint => {
   }
 }
 
-export const mostSignificantBit = (x: bigint): bigint => {
-  return BigInt(Math.floor(Math.log2(Number(x))))
-}
+export const lnWad = (x: bigint): bigint => {
+  const or = (a: bigint, b: bigint) => a | b
+  const xor = (a: bigint, b: bigint) => a ^ b
+  const and = (a: bigint, b: bigint) => a & b
+  const add = (a: bigint, b: bigint) => a + b
+  const sub = (a: bigint, b: bigint) => a - b
+  const mul = (a: bigint, b: bigint) => a * b
+  const sdiv = (a: bigint, b: bigint) => a / b
+  const lt = (a: bigint, b: bigint) => (a < b ? 1n : 0n)
+  const shl = (x: bigint, y: bigint) => y << x
+  const shr = (x: bigint, y: bigint) => y >> x
+  const sar = (x: bigint, y: bigint) => y >> x
+  const byte = (i: bigint, x: bigint) => (x >> (248n - i * 8n)) & 0xffn
 
-export const log2 = (x: bigint): bigint => {
-  const msb = mostSignificantBit(x)
+  // We want to convert `x` from `10**18` fixed point to `2**96` fixed point.
+  // We do this by multiplying by `2**96 / 10**18`. But since
+  // `ln(x * C) = ln(x) + ln(C)`, we can simply do nothing here
+  // and add `ln(2**96 / 10**18)` at the end.
 
-  if (msb > 128n) {
-    x >>= msb - 128n
-  } else if (msb < 128n) {
-    x <<= 128n - msb
-  }
+  // Compute `k = log2(x) - 96`, `r = 159 - k = 255 - log2(x) = 255 ^ log2(x)`.
+  let r = shl(7n, lt(0xffffffffffffffffffffffffffffffffn, x))
+  r = or(r, shl(6n, lt(0xffffffffffffffffn, shr(r, x))))
+  r = or(r, shl(5n, lt(0xffffffffn, shr(r, x))))
+  r = or(r, shl(4n, lt(0xffffn, shr(r, x))))
+  r = or(r, shl(3n, lt(0xffn, shr(r, x))))
+  // forgefmt: disable-next-item
+  r = xor(
+    r,
+    byte(
+      and(0x1fn, shr(shr(r, x), 0x8421084210842108cc6318c6db6d54ben)),
+      0xf8f9f9faf9fdfafbf9fdfcfdfafbfcfef9fafdfafcfcfbfefafafcfbffffffffn,
+    ),
+  )
 
-  x &= 0xffffffffffffffffffffffffffffffffn
+  // Reduce range of x to (1, 2) * 2**96
+  // ln(2^k * x) = k * ln(2) + ln(x)
+  x = shr(159n, shl(r, x))
 
-  let result = (msb - 128n) << 128n
-  let bit = 0x80000000000000000000000000000000n
-  for (let i = 0n; i < 128n && x > 0n; i++) {
-    x = (x << 1n) + ((x * x + 0x80000000000000000000000000000000n) >> 128n)
-    if (x > 0xffffffffffffffffffffffffffffffffn) {
-      result |= bit
-      x = (x >> 1n) - 0x80000000000000000000000000000000n
-    }
-    bit >>= 1n
-  }
+  // Evaluate using a (8, 8)-term rational approximation.
+  // `p` is made monic, we will multiply by a scale factor later.
+  // forgefmt: disable-next-item
+  let p = sub(
+    // This heavily nested expression is to avoid stack-too-deep for via-ir.
+    sar(
+      96n,
+      mul(
+        add(
+          43456485725739037958740375743393n,
+          sar(
+            96n,
+            mul(
+              add(
+                24828157081833163892658089445524n,
+                sar(96n, mul(add(3273285459638523848632254066296n, x), x)),
+              ),
+              x,
+            ),
+          ),
+        ),
+        x,
+      ),
+    ),
+    11111509109440967052023855526967n,
+  )
+  p = sub(sar(96n, mul(p, x)), 45023709667254063763336534515857n)
+  p = sub(sar(96n, mul(p, x)), 14706773417378608786704636184526n)
+  p = sub(mul(p, x), shl(96n, 795164235651350426258249787498n))
+  // We leave `p` in `2**192` basis so we don't need to scale it back up for the division.
 
-  return result
+  // `q` is monic by convention.
+  let q = add(5573035233440673466300451813936n, x)
+  q = add(71694874799317883764090561454958n, sar(96n, mul(x, q)))
+  q = add(283447036172924575727196451306956n, sar(96n, mul(x, q)))
+  q = add(401686690394027663651624208769553n, sar(96n, mul(x, q)))
+  q = add(204048457590392012362485061816622n, sar(96n, mul(x, q)))
+  q = add(31853899698501571402653359427138n, sar(96n, mul(x, q)))
+  q = add(909429971244387300277376558375n, sar(96n, mul(x, q)))
+
+  // `p / q` is in the range `(0, 0.125) * 2**96`.
+
+  // Finalization, we need to:
+  // - Multiply by the scale factor `s = 5.549…`.
+  // - Add `ln(2**96 / 10**18)`.
+  // - Add `k * ln(2)`.
+  // - Multiply by `10**18 / 2**96 = 5**18 >> 78`.
+
+  // The q polynomial is known not to have zeros in the domain.
+  // No scaling required because p is already `2**96` too large.
+  p = sdiv(p, q)
+  // Multiply by the scaling factor: `s * 5**18 * 2**96`, base is now `5**18 * 2**192`.
+  p = mul(1677202110996718588342820967067443963516166n, p)
+  // Add `ln(2) * k * 5**18 * 2**192`.
+  // forgefmt: disable-next-item
+  p = add(
+    mul(
+      16597577552685614221487285958193947469193820559219878177908093499208371n,
+      sub(159n, r),
+    ),
+    p,
+  )
+  // Base conversion: mul `2**96 / (5**18 * 2**192)`.
+  r = sdiv(p, 302231454903657293676544000000000000000000n)
+
+  return r
 }
---
+++ test/math.test.ts
@@ -0,0 +1,95 @@
+import { expect, test } from 'vitest'
+import { createPublicClient, getAddress, http } from 'viem'
+import { arbitrumSepolia } from 'viem/chains'
+
+import { lnWad } from '../src/utils/math'
+
+const _abi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'a',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'b',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bool',
+        name: 'roundingUp',
+        type: 'bool',
+      },
+    ],
+    name: 'divide',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'int256',
+        name: 'x',
+        type: 'int256',
+      },
+    ],
+    name: 'lnWad',
+    outputs: [
+      {
+        internalType: 'int256',
+        name: '',
+        type: 'int256',
+      },
+    ],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+] as const
+
+const MATH_WRAPPER_ADDRESS = '0x98d55f0AaEda3bad7815C8F3d8f8882e6B702D26'
+
+const randomInteger = (start: number, end: number) => {
+  return Math.floor(Math.random() * (end - start + 1) + start)
+}
+
+const MIN_PRICE = 1350587n
+const MAX_PRICE = 4647684107270898330752324302845848816923571339324334n
+
+const publicClient = createPublicClient({
+  chain: arbitrumSepolia,
+  transport: http(),
+})
+
+test('lnWad', async () => {
+  const randomValues = [
+    MIN_PRICE,
+    ...Array.from({ length: 500 }, () =>
+      randomInteger(Number(MIN_PRICE + 1n), Number(MAX_PRICE - 1n)),
+    ),
+    MAX_PRICE,
+  ]
+  const actualValues = (
+    (await publicClient.multicall({
+      contracts: randomValues.map((value) => ({
+        address: getAddress(MATH_WRAPPER_ADDRESS),
+        abi: _abi,
+        functionName: 'lnWad',
+        args: [value],
+      })),
+    })) as { result: bigint }[]
+  ).map(({ result }) => result)
+
+  const expectedValues = randomValues.map((priceIndex) =>
+    lnWad(BigInt(priceIndex)),
+  )
+  expect(expectedValues).toEqual(actualValues)
+})
