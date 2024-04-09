7017acbc2a73a3035c7759052cad5321c8ea6f2d
graykode <nlkey2022@gmail.com>
2024-04-09 11:54:19 +0900

    test: add toBookId test

---
+++ test/book-id.test.ts
@@ -0,0 +1,106 @@
+import { expect, test } from 'vitest'
+import { createPublicClient, http } from 'viem'
+import { arbitrumSepolia } from 'viem/chains'
+
+import { toBookId } from '../src/utils/book-id'
+import {
+  MAKER_DEFAULT_POLICY,
+  TAKER_DEFAULT_POLICY,
+} from '../src/constants/fee'
+
+import { BOOK_ID_WRAPPER_ADDRESS } from './utils/addresses'
+
+const _abi = [
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: 'Currency',
+            name: 'base',
+            type: 'address',
+          },
+          {
+            internalType: 'uint64',
+            name: 'unit',
+            type: 'uint64',
+          },
+          {
+            internalType: 'Currency',
+            name: 'quote',
+            type: 'address',
+          },
+          {
+            internalType: 'FeePolicy',
+            name: 'makerPolicy',
+            type: 'uint24',
+          },
+          {
+            internalType: 'contract IHooks',
+            name: 'hooks',
+            type: 'address',
+          },
+          {
+            internalType: 'FeePolicy',
+            name: 'takerPolicy',
+            type: 'uint24',
+          },
+        ],
+        internalType: 'struct IBookManager.BookKey',
+        name: 'bookKey',
+        type: 'tuple',
+      },
+    ],
+    name: 'toId',
+    outputs: [
+      {
+        internalType: 'BookId',
+        name: 'id',
+        type: 'uint192',
+      },
+    ],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+] as const
+
+const publicClient = createPublicClient({
+  chain: arbitrumSepolia,
+  transport: http(),
+})
+
+test('check toBookId function', async () => {
+  const bidBookKey = {
+    base: '0x0000000000000000000000000000000000000000' as `0x${string}`,
+    unit: 10n ** 12n,
+    quote: '0x447ad4a108b5540c220f9f7e83723ac87c0f8fd8' as `0x${string}`,
+    makerPolicy: Number(MAKER_DEFAULT_POLICY.value),
+    hooks: '0x0000000000000000000000000000000000000000' as `0x${string}`,
+    takerPolicy: Number(TAKER_DEFAULT_POLICY.value),
+  }
+  expect(
+    await publicClient.readContract({
+      address: BOOK_ID_WRAPPER_ADDRESS,
+      abi: _abi,
+      functionName: 'toId',
+      args: [bidBookKey],
+    }),
+  ).toBe(toBookId(bidBookKey.quote, bidBookKey.base, bidBookKey.unit))
+
+  const askBookKey = {
+    base: '0x447ad4a108b5540c220f9f7e83723ac87c0f8fd8' as `0x${string}`,
+    unit: 10n ** 1n,
+    quote: '0x0000000000000000000000000000000000000000' as `0x${string}`,
+    makerPolicy: Number(MAKER_DEFAULT_POLICY.value),
+    hooks: '0x0000000000000000000000000000000000000000' as `0x${string}`,
+    takerPolicy: Number(TAKER_DEFAULT_POLICY.value),
+  }
+  expect(
+    await publicClient.readContract({
+      address: BOOK_ID_WRAPPER_ADDRESS,
+      abi: _abi,
+      functionName: 'toId',
+      args: [askBookKey],
+    }),
+  ).toBe(toBookId(askBookKey.quote, askBookKey.base, askBookKey.unit))
+})
