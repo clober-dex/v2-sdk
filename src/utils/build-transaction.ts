7aa7a1752862c669a15740a166d286f7499b8172
graykode <nlkey2022@gmail.com>
2024-04-19 00:00:56 +0900

    feat: set gas limit

--- src/utils/build-transaction.ts
+++ src/utils/build-transaction.ts
@@ -10,15 +10,17 @@ import { cachedPublicClients } from '../constants/client'
 export const buildTransaction = async (
   chainId: CHAIN_IDS,
   args: WriteContractParameters | SimulateContractParameters,
+  gasLimit?: bigint,
 ): Promise<Transaction> => {
   const data = encodeFunctionData(args)
   const [gas, gasPrice] = await Promise.all([
-    cachedPublicClients[chainId]!.estimateGas({
-      account: args.account,
-      data,
-      to: args.address,
-      value: args.value || 0n,
-    }),
+    gasLimit ??
+      cachedPublicClients[chainId]!.estimateGas({
+        account: args.account,
+        data,
+        to: args.address,
+        value: args.value || 0n,
+      }),
     cachedPublicClients[chainId]!.getGasPrice(),
   ])
   return {
