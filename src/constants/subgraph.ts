537bde9816bd37d6c519924738e4c3e79ba4477a
graykode <nlkey2022@gmail.com>
2024-04-29 20:59:47 +0900

    feat: update subgraph and currency addresses

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -5,6 +5,10 @@ const SUBGRAPH_URL: {
 } = {
   [CHAIN_IDS.ARBITRUM_SEPOLIA]:
     'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-core-subgraph/api',
+  [CHAIN_IDS.BASE]:
+    'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-core-subgraph-base/api',
+  [CHAIN_IDS.BERACHAIN_TESTNET]:
+    'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/v2-core-subgraph/v1.2.0/gn',
 }
 
 class Subgraph {
