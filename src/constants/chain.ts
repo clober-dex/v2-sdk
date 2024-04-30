0b67d480ef4eed4129834424db72c6e395de3148
graykode <nlkey2022@gmail.com>
2024-04-30 11:27:55 +0900

    feat: add multicall3 for bera testnet as manually

--- src/constants/chain.ts
+++ src/constants/chain.ts
@@ -16,5 +16,13 @@ export const CHAIN_MAP: {
 } = {
   [CHAIN_IDS.ARBITRUM_SEPOLIA]: arbitrumSepolia,
   [CHAIN_IDS.BASE]: base,
-  [CHAIN_IDS.BERACHAIN_TESTNET]: berachainTestnet,
+  [CHAIN_IDS.BERACHAIN_TESTNET]: {
+    ...berachainTestnet,
+    contracts: {
+      multicall3: {
+        address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+        blockCreated: 866924,
+      },
+    },
+  },
 }
