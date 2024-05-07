aa6ca0bc731225f8c03ae80e96d2808687a3dc17
graykode <nlkey2022@gmail.com>
2024-05-07 13:58:10 +0900

    fix: number loss

--- test/utils/depth.ts
+++ test/utils/depth.ts
@@ -1,13 +1,14 @@
 import { Depth } from '@clober/v2-sdk'
+import BigNumber from 'bignumber.js'
 
 export const getSize = (depth: Depth[], from: number, to: number) => {
-  return Number(
+  return new BigNumber(
     (
       depth.find(
         ({ price }) => from <= Number(price) && Number(price) <= to,
       ) ?? {
         baseAmount: 0n,
       }
-    ).baseAmount,
+    ).baseAmount.toString(),
   )
 }
