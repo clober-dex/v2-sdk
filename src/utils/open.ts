885628eb781ed20bb8ddd2e7fcd393fa0b537166
graykode <nlkey2022@gmail.com>
2024-07-10 15:25:35 +0900

    perf: cache isOpened

--- src/utils/open.ts
+++ src/utils/open.ts
@@ -24,11 +24,28 @@ const _abi = [
   },
 ] as const
 
+const buildBookCacheKey = (chainId: CHAIN_IDS, bookId: bigint) =>
+  `${chainId}:${bookId}`
+const isOpenedCache = new Map<string, boolean>()
+const getIsOpenedFromCache = (chainId: CHAIN_IDS, bookId: bigint) =>
+  isOpenedCache.get(buildBookCacheKey(chainId, bookId))
+const setIsOpenedToCache = (
+  chainId: CHAIN_IDS,
+  bookId: bigint,
+  isOpened: boolean,
+) => isOpenedCache.set(buildBookCacheKey(chainId, bookId), isOpened)
+
 export async function fetchIsOpened(chainId: CHAIN_IDS, bookId: bigint) {
-  return cachedPublicClients[chainId].readContract({
+  const cachedIsOpened = getIsOpenedFromCache(chainId, bookId)
+  if (cachedIsOpened !== undefined) {
+    return cachedIsOpened
+  }
+  const isOpened = await cachedPublicClients[chainId].readContract({
     address: CONTRACT_ADDRESSES[chainId]!.BookManager,
     abi: _abi,
     functionName: 'isOpened',
     args: [bookId],
   })
+  setIsOpenedToCache(chainId, bookId, isOpened)
+  return isOpened
 }
