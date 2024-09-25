4336d604b722dd3e4a0924e41850a51cc446c625
graykode <nlkey2022@gmail.com>
2024-09-25 15:25:18 +0900

    fix: fix `emptyERC20PermitParams`

--- src/constants/permit.ts
+++ src/constants/permit.ts
@@ -1,9 +1,11 @@
+import { zeroHash } from 'viem'
+
 export const emptyERC20PermitParams = {
   permitAmount: 0n,
   signature: {
     deadline: 0n,
     v: 0,
-    r: '0x',
-    s: '0x',
+    r: zeroHash,
+    s: zeroHash,
   },
 }
