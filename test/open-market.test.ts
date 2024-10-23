a3a297e77ac3af6c3d9cb3920b83a36acc3a5d9b
graykode <nlkey2022@gmail.com>
2024-10-23 16:19:42 +0900

    test: fix open tests

--- test/open-market.test.ts
+++ test/open-market.test.ts
@@ -1,6 +1,5 @@
-import { expect, test, afterEach } from 'vitest'
-import { getMarket, getPool, openMarket, openPool } from '@clober/v2-sdk'
-import { zeroHash } from 'viem'
+import { afterEach, expect, test } from 'vitest'
+import { getMarket, openMarket } from '@clober/v2-sdk'
 
 import { cloberTestChain } from '../src/constants/test-chain'
 
@@ -96,69 +95,4 @@ test('try already open market', async () => {
     },
   })
   expect(transaction).toBeUndefined()
-})
-
-test('try open pool', async () => {
-  await Promise.all(
-    clients.map(({ testClient }) => {
-      return testClient.reset({
-        jsonRpcUrl: FORK_URL,
-        blockNumber: 83889075n,
-      })
-    }),
-  )
-  const { publicClient, walletClient } = clients[0] as any
-
-  const transaction1 = await openPool({
-    chainId: cloberTestChain.id,
-    userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    tokenA: '0xEfC8df673Ac18CFa6b92A1eE8939C84506C9Faf3',
-    tokenB: '0x0000000000000000000000000000000000000000',
-    salt: zeroHash,
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-    },
-  })
-  const beforePool = await getPool({
-    chainId: cloberTestChain.id,
-    token0: '0xEfC8df673Ac18CFa6b92A1eE8939C84506C9Faf3',
-    token1: '0x0000000000000000000000000000000000000000',
-    salt: zeroHash,
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-    },
-  })
-  expect(beforePool.isOpened).toEqual(false)
-
-  await walletClient.sendTransaction({
-    ...transaction1!,
-    account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    gasPrice: transaction1!.gasPrice! * 2n,
-  })
-  const afterPool = await getPool({
-    chainId: cloberTestChain.id,
-    token0: '0xEfC8df673Ac18CFa6b92A1eE8939C84506C9Faf3',
-    token1: '0x0000000000000000000000000000000000000000',
-    salt: zeroHash,
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-    },
-  })
-  expect(afterPool.isOpened).toEqual(true)
-
-  const transaction2 = await openPool({
-    chainId: cloberTestChain.id,
-    userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    tokenA: '0xEfC8df673Ac18CFa6b92A1eE8939C84506C9Faf3',
-    tokenB: '0x0000000000000000000000000000000000000000',
-    salt: zeroHash,
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-    },
-  })
-  expect(transaction2).toBeUndefined()
 })
