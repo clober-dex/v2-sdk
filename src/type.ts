8498903e95f8a9417f82c710809d044b7d67acfc
graykode <nlkey2022@gmail.com>
2024-12-25 17:53:20 +0900

    fix: remove `type`

--- src/type.ts
+++ src/type.ts
@@ -4,9 +4,9 @@ import { CHAIN_IDS } from './constants/chain'
 import type { Currency, Currency6909 } from './model/currency'
 
 export { CHAIN_IDS } from './constants/chain'
+export { FeePolicy } from './model/fee-policy'
 export type { Currency } from './model/currency'
 export type { OpenOrder } from './model/open-order'
-export type { FeePolicy } from './model/fee-policy'
 
 export type Depth = {
   price: string
