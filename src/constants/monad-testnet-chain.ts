aad40ec67fe325fb79dce1eab7aae2e5e13be434
graykode <nlkey2022@gmail.com>
2025-02-15 00:51:00 +0900

    feat: update multicall contract address

--- src/constants/monad-testnet-chain.ts
+++ src/constants/monad-testnet-chain.ts
@@ -3,6 +3,7 @@ import { defineChain } from 'viem'
 export const monadTestnet = /*#__PURE__*/ defineChain({
   id: 10143,
   name: 'Monad Testnet',
+  network: 'monad',
   nativeCurrency: {
     decimals: 18,
     name: 'MON',
@@ -10,7 +11,14 @@ export const monadTestnet = /*#__PURE__*/ defineChain({
   },
   rpcUrls: {
     default: {
-      http: ['https://monad-api.blockvision.org/testnet/api'],
+      http: [
+        'https://testnet-rpc2.monad.xyz/52227f026fa8fac9e2014c58fbf5643369b3bfc6',
+      ],
+    },
+    public: {
+      http: [
+        'https://testnet-rpc2.monad.xyz/52227f026fa8fac9e2014c58fbf5643369b3bfc6',
+      ],
     },
   },
   blockExplorers: {
@@ -22,8 +30,8 @@ export const monadTestnet = /*#__PURE__*/ defineChain({
   testnet: true,
   contracts: {
     multicall3: {
-      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
-      blockCreated: 251449,
+      address: '0x6cEfcd4DCA776FFaBF6E244616ea573e4d646566',
+      blockCreated: 42209,
     },
   },
 })
