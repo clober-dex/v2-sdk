2d81edccf213f0353e7092c15749ffd784743070
graykode <nlkey2022@gmail.com>
2025-04-18 23:48:37 +0900

    feat: remove unused chains

--- src/constants/chain.ts
+++ src/constants/chain.ts
@@ -1,16 +1,13 @@
 import {
   arbitrumSepolia,
   base,
+  berachain,
   type Chain,
-  mitosisTestnet,
   monadTestnet,
   sonic,
-  zkSync,
-  berachain,
 } from 'viem/chains'
 
 import { cloberTestChain, cloberTestChain2 } from './test-chain'
-import { berachainBartioTestnet } from './bera-bartio-chain'
 import { riseSepolia } from './rise-sepolia'
 
 // follow the https://docs.alchemy.com/reference/supported-subgraph-chains
@@ -20,11 +17,8 @@ export enum CHAIN_IDS {
   ARBITRUM_SEPOLIA = arbitrumSepolia.id,
   BASE = base.id,
   BERACHAIN_MAINNET = berachain.id,
-  BERACHAIN_TESTNET = berachainBartioTestnet.id,
-  MITOSIS_TESTNET = mitosisTestnet.id,
   MONAD_TESTNET = monadTestnet.id,
   SONIC_MAINNET = sonic.id,
-  ZKSYNC_ERA = zkSync.id,
   RISE_SEPOLIA = riseSepolia.id,
 }
 
@@ -36,11 +30,8 @@ export const CHAIN_MAP: {
   [CHAIN_IDS.ARBITRUM_SEPOLIA]: arbitrumSepolia,
   [CHAIN_IDS.BASE]: base,
   [CHAIN_IDS.BERACHAIN_MAINNET]: berachain,
-  [CHAIN_IDS.BERACHAIN_TESTNET]: berachainBartioTestnet,
-  [CHAIN_IDS.MITOSIS_TESTNET]: mitosisTestnet,
   [CHAIN_IDS.MONAD_TESTNET]: monadTestnet,
   [CHAIN_IDS.SONIC_MAINNET]: sonic,
-  [CHAIN_IDS.ZKSYNC_ERA]: zkSync,
   [CHAIN_IDS.RISE_SEPOLIA]: riseSepolia,
 }
 
@@ -48,7 +39,5 @@ export const isTestnetChain = (chainId: CHAIN_IDS): boolean =>
   chainId === CHAIN_IDS.CLOBER_TESTNET ||
   chainId === CHAIN_IDS.CLOBER_TESTNET_2 ||
   chainId === CHAIN_IDS.ARBITRUM_SEPOLIA ||
-  chainId === CHAIN_IDS.BERACHAIN_TESTNET ||
-  chainId === CHAIN_IDS.MITOSIS_TESTNET ||
   chainId === CHAIN_IDS.MONAD_TESTNET ||
   chainId === CHAIN_IDS.RISE_SEPOLIA
