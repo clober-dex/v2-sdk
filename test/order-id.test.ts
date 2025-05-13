9499a0f9fa141978f7d592f827026d17248f98c5
graykode <nlkey2022@gmail.com>
2025-05-13 17:21:15 +0900

    test: update fromOrderId tests to use BigInt for numeric values

--- test/order-id.test.ts
+++ test/order-id.test.ts
@@ -8,10 +8,10 @@ test('check fromOrderId function - 1', async () => {
       53566444723624360785422944131996696091141564570442382186621897550883387867403n,
     )
   expect(bookId).toEqual(
-    '2903842787083910905150096686205997338709207897290567260368',
+    2903842787083910905150096686205997338709207897290567260368n,
   )
-  expect(tick).toEqual(-259218)
-  expect(index).toEqual(267)
+  expect(tick).toEqual(-259218n)
+  expect(index).toEqual(267n)
 })
 
 test('check fromOrderId function - 2', async () => {
@@ -20,8 +20,8 @@ test('check fromOrderId function - 2', async () => {
       53566444723624360785422944131996696091141564570442382186622182562989005078795n,
     )
   expect(bookId).toEqual(
-    '2903842787083910905150096686205997338709207897290567260368',
+    2903842787083910905150096686205997338709207897290567260368n,
   )
-  expect(tick).toEqual(-1)
-  expect(index).toEqual(267)
+  expect(tick).toEqual(-1n)
+  expect(index).toEqual(267n)
 })
