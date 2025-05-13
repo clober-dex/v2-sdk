5ec3b9259443d9142f216b7d5a84e1333c3b901c
graykode <nlkey2022@gmail.com>
2025-05-13 17:18:01 +0900

    refactor: inline ABI definitions in fetchAllowance and fetchIsApprovedForAll functions

--- src/apis/allowance.ts
+++ src/apis/allowance.ts
@@ -1,32 +1,5 @@
 import { PublicClient } from 'viem'
 
-const _abi = [
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'owner',
-        type: 'address',
-      },
-      {
-        internalType: 'address',
-        name: 'spender',
-        type: 'address',
-      },
-    ],
-    name: 'allowance',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-] as const
-
 export async function fetchAllowance(
   publicClient: PublicClient,
   token: `0x${string}`,
@@ -35,7 +8,32 @@ export async function fetchAllowance(
 ): Promise<bigint> {
   return publicClient.readContract({
     address: token,
-    abi: _abi,
+    abi: [
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'owner',
+            type: 'address',
+          },
+          {
+            internalType: 'address',
+            name: 'spender',
+            type: 'address',
+          },
+        ],
+        name: 'allowance',
+        outputs: [
+          {
+            internalType: 'uint256',
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ] as const,
     functionName: 'allowance',
     args: [userAddress, spenderAddress],
   })
--- src/apis/approval.ts
+++ src/apis/approval.ts
@@ -3,33 +3,6 @@ import { PublicClient } from 'viem'
 import { CHAIN_IDS } from '../constants/chain'
 import { CONTRACT_ADDRESSES } from '../constants/addresses'
 
-const _abi = [
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'owner',
-        type: 'address',
-      },
-      {
-        internalType: 'address',
-        name: 'operator',
-        type: 'address',
-      },
-    ],
-    name: 'isApprovedForAll',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-] as const
-
 export async function fetchIsApprovedForAll(
   publicClient: PublicClient,
   chainId: CHAIN_IDS,
@@ -37,7 +10,32 @@ export async function fetchIsApprovedForAll(
 ): Promise<boolean> {
   return publicClient.readContract({
     address: CONTRACT_ADDRESSES[chainId]!.BookManager,
-    abi: _abi,
+    abi: [
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'owner',
+            type: 'address',
+          },
+          {
+            internalType: 'address',
+            name: 'operator',
+            type: 'address',
+          },
+        ],
+        name: 'isApprovedForAll',
+        outputs: [
+          {
+            internalType: 'bool',
+            name: '',
+            type: 'bool',
+          },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ] as const,
     functionName: 'isApprovedForAll',
     args: [owner, CONTRACT_ADDRESSES[chainId]!.Controller],
   })
