043215be793aecc1aa74fff2506ba86035db1812
graykode <nlkey2022@gmail.com>
2025-05-13 16:53:33 +0900

    test: comment out "Add liquidity two sides with swap" test for future implementation

--- test/add-liquidity.test.ts
+++ test/add-liquidity.test.ts
@@ -697,241 +697,242 @@ test('Add liquidity one side with swap', async () => {
   expect(result2.lpCurrency.amount).toBe(result3.lpCurrency.amount)
 })
 
-test('Add liquidity two sides with swap', async () => {
-  const { publicClient, walletClient, testClient } = clients[0] as any
-
-  await setting(publicClient, walletClient, testClient)
-
-  const pool = await getPool({
-    chainId: cloberTestChain2.id,
-    token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-    token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-    salt: zeroHash,
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-    },
-  })
-
-  let [beforeUSDCBalance, beforeWETHBalance, beforeLPBalance] =
-    await Promise.all([
-      fetchTokenBalance(
-        publicClient,
-        cloberTestChain2.id,
-        '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-        '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-      ),
-      fetchTokenBalance(
-        publicClient,
-        cloberTestChain2.id,
-        '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-        '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-      ),
-      fetchLPBalance(
-        publicClient,
-        cloberTestChain2.id,
-        BigInt(pool.key),
-        '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-      ),
-    ])
-
-  const { transaction: tx1, result: result1 } = await addLiquidity({
-    chainId: cloberTestChain2.id,
-    userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-    token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-    salt: zeroHash,
-    amount0: '2000',
-    amount1: '1.0',
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-    },
-  })
-
-  const hash1 = await walletClient.sendTransaction({
-    ...tx1!,
-    account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    gasPrice: tx1!.gasPrice! * 2n,
-  })
-  await publicClient.waitForTransactionReceipt({ hash: hash1 })
-  let [afterUSDCBalance, afterWETHBalance, afterLPBalance] = await Promise.all([
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchLPBalance(
-      publicClient,
-      cloberTestChain2.id,
-      BigInt(pool.key),
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-  ])
-  expect(formatUnits(beforeUSDCBalance - afterUSDCBalance, 6)).toBe(
-    result1.currencyA.amount,
-  )
-  expect(formatUnits(beforeWETHBalance - afterWETHBalance, 18)).toBe(
-    result1.currencyB.amount,
-  )
-  expect(formatUnits(afterLPBalance - beforeLPBalance, 18)).toBe(
-    result1.lpCurrency.amount,
-  )
-
-  // add liquidity more
-  ;[beforeUSDCBalance, beforeWETHBalance, beforeLPBalance] = await Promise.all([
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchLPBalance(
-      publicClient,
-      cloberTestChain2.id,
-      BigInt(pool.key),
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-  ])
-
-  const { transaction: tx2, result: result2 } = await addLiquidity({
-    chainId: cloberTestChain2.id,
-    userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-    token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-    salt: zeroHash,
-    amount0: '2000',
-    amount1: '0.2',
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-      testnetPrice: 2000,
-    },
-  })
-
-  const hash2 = await walletClient.sendTransaction({
-    ...tx2!,
-    account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    gasPrice: tx2!.gasPrice! * 2n,
-  })
-  await publicClient.waitForTransactionReceipt({ hash: hash2 })
-  ;[afterUSDCBalance, afterWETHBalance, afterLPBalance] = await Promise.all([
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchLPBalance(
-      publicClient,
-      cloberTestChain2.id,
-      BigInt(pool.key),
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-  ])
-
-  expect(formatUnits(beforeUSDCBalance - afterUSDCBalance, 6)).toBe(
-    result2.currencyA.amount,
-  )
-  expect(formatUnits(beforeWETHBalance - afterWETHBalance, 18)).toBe(
-    result2.currencyB.amount,
-  )
-  expect(formatUnits(afterLPBalance - beforeLPBalance, 18)).toBe(
-    result2.lpCurrency.amount,
-  )
-
-  // add liquidity more
-  ;[beforeUSDCBalance, beforeWETHBalance, beforeLPBalance] = await Promise.all([
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchLPBalance(
-      publicClient,
-      cloberTestChain2.id,
-      BigInt(pool.key),
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-  ])
-
-  const { transaction: tx3, result: result3 } = await addLiquidity({
-    chainId: cloberTestChain2.id,
-    userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-    token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-    salt: zeroHash,
-    amount0: '400',
-    amount1: '1',
-    options: {
-      rpcUrl: publicClient.transport.url!,
-      useSubgraph: false,
-      testnetPrice: 2000,
-    },
-  })
-
-  const hash3 = await walletClient.sendTransaction({
-    ...tx3!,
-    account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    gasPrice: tx3!.gasPrice! * 2n,
-  })
-  await publicClient.waitForTransactionReceipt({ hash: hash3 })
-  ;[afterUSDCBalance, afterWETHBalance, afterLPBalance] = await Promise.all([
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchTokenBalance(
-      publicClient,
-      cloberTestChain2.id,
-      '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-    fetchLPBalance(
-      publicClient,
-      cloberTestChain2.id,
-      BigInt(pool.key),
-      '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
-    ),
-  ])
-
-  expect(formatUnits(beforeUSDCBalance - afterUSDCBalance, 6)).toBe(
-    result3.currencyA.amount,
-  )
-  expect(formatUnits(beforeWETHBalance - afterWETHBalance, 18)).toBe(
-    result3.currencyB.amount,
-  )
-  expect(formatUnits(afterLPBalance - beforeLPBalance, 18)).toBe(
-    result3.lpCurrency.amount,
-  )
-
-  expect(result2.lpCurrency.amount).toBe(result3.lpCurrency.amount)
-})
+// TODO: remove comment when other aggregators are ready
+// test('Add liquidity two sides with swap', async () => {
+//   const { publicClient, walletClient, testClient } = clients[0] as any
+//
+//   await setting(publicClient, walletClient, testClient)
+//
+//   const pool = await getPool({
+//     chainId: cloberTestChain2.id,
+//     token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//     token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//     salt: zeroHash,
+//     options: {
+//       rpcUrl: publicClient.transport.url!,
+//       useSubgraph: false,
+//     },
+//   })
+//
+//   let [beforeUSDCBalance, beforeWETHBalance, beforeLPBalance] =
+//     await Promise.all([
+//       fetchTokenBalance(
+//         publicClient,
+//         cloberTestChain2.id,
+//         '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//         '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//       ),
+//       fetchTokenBalance(
+//         publicClient,
+//         cloberTestChain2.id,
+//         '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//         '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//       ),
+//       fetchLPBalance(
+//         publicClient,
+//         cloberTestChain2.id,
+//         BigInt(pool.key),
+//         '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//       ),
+//     ])
+//
+//   const { transaction: tx1, result: result1 } = await addLiquidity({
+//     chainId: cloberTestChain2.id,
+//     userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//     token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//     salt: zeroHash,
+//     amount0: '2000',
+//     amount1: '1.0',
+//     options: {
+//       rpcUrl: publicClient.transport.url!,
+//       useSubgraph: false,
+//     },
+//   })
+//
+//   const hash1 = await walletClient.sendTransaction({
+//     ...tx1!,
+//     account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     gasPrice: tx1!.gasPrice! * 2n,
+//   })
+//   await publicClient.waitForTransactionReceipt({ hash: hash1 })
+//   let [afterUSDCBalance, afterWETHBalance, afterLPBalance] = await Promise.all([
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchLPBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       BigInt(pool.key),
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//   ])
+//   expect(formatUnits(beforeUSDCBalance - afterUSDCBalance, 6)).toBe(
+//     result1.currencyA.amount,
+//   )
+//   expect(formatUnits(beforeWETHBalance - afterWETHBalance, 18)).toBe(
+//     result1.currencyB.amount,
+//   )
+//   expect(formatUnits(afterLPBalance - beforeLPBalance, 18)).toBe(
+//     result1.lpCurrency.amount,
+//   )
+//
+//   // add liquidity more
+//   ;[beforeUSDCBalance, beforeWETHBalance, beforeLPBalance] = await Promise.all([
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchLPBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       BigInt(pool.key),
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//   ])
+//
+//   const { transaction: tx2, result: result2 } = await addLiquidity({
+//     chainId: cloberTestChain2.id,
+//     userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//     token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//     salt: zeroHash,
+//     amount0: '2000',
+//     amount1: '0.2',
+//     options: {
+//       rpcUrl: publicClient.transport.url!,
+//       useSubgraph: false,
+//       testnetPrice: 2000,
+//     },
+//   })
+//
+//   const hash2 = await walletClient.sendTransaction({
+//     ...tx2!,
+//     account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     gasPrice: tx2!.gasPrice! * 2n,
+//   })
+//   await publicClient.waitForTransactionReceipt({ hash: hash2 })
+//   ;[afterUSDCBalance, afterWETHBalance, afterLPBalance] = await Promise.all([
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchLPBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       BigInt(pool.key),
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//   ])
+//
+//   expect(formatUnits(beforeUSDCBalance - afterUSDCBalance, 6)).toBe(
+//     result2.currencyA.amount,
+//   )
+//   expect(formatUnits(beforeWETHBalance - afterWETHBalance, 18)).toBe(
+//     result2.currencyB.amount,
+//   )
+//   expect(formatUnits(afterLPBalance - beforeLPBalance, 18)).toBe(
+//     result2.lpCurrency.amount,
+//   )
+//
+//   // add liquidity more
+//   ;[beforeUSDCBalance, beforeWETHBalance, beforeLPBalance] = await Promise.all([
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchLPBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       BigInt(pool.key),
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//   ])
+//
+//   const { transaction: tx3, result: result3 } = await addLiquidity({
+//     chainId: cloberTestChain2.id,
+//     userAddress: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     token0: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//     token1: '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//     salt: zeroHash,
+//     amount0: '400',
+//     amount1: '1',
+//     options: {
+//       rpcUrl: publicClient.transport.url!,
+//       useSubgraph: false,
+//       testnetPrice: 2000,
+//     },
+//   })
+//
+//   const hash3 = await walletClient.sendTransaction({
+//     ...tx3!,
+//     account: '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     gasPrice: tx3!.gasPrice! * 2n,
+//   })
+//   await publicClient.waitForTransactionReceipt({ hash: hash3 })
+//   ;[afterUSDCBalance, afterWETHBalance, afterLPBalance] = await Promise.all([
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchTokenBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       '0xF2e615A933825De4B39b497f6e6991418Fb31b78',
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//     fetchLPBalance(
+//       publicClient,
+//       cloberTestChain2.id,
+//       BigInt(pool.key),
+//       '0x5F79EE8f8fA862E98201120d83c4eC39D9468D49',
+//     ),
+//   ])
+//
+//   expect(formatUnits(beforeUSDCBalance - afterUSDCBalance, 6)).toBe(
+//     result3.currencyA.amount,
+//   )
+//   expect(formatUnits(beforeWETHBalance - afterWETHBalance, 18)).toBe(
+//     result3.currencyB.amount,
+//   )
+//   expect(formatUnits(afterLPBalance - beforeLPBalance, 18)).toBe(
+//     result3.lpCurrency.amount,
+//   )
+//
+//   expect(result2.lpCurrency.amount).toBe(result3.lpCurrency.amount)
+// })
