8ce616d9dceece7ef32507dd2d16e12a171891ce
graykode <nlkey2022@gmail.com>
2025-05-13 17:17:46 +0900

    test: add tests for fromOrderId function with specific cases

--- test/order-id.test.ts
+++ test/order-id.test.ts
@@ -1,63 +1,27 @@
-// import { createPublicClient, http } from 'viem'
-// import { arbitrumSepolia } from 'viem/chains'
-//
-// import { FORK_URL } from './utils/constants'
-//
-// const _abi = [
-//   {
-//     inputs: [
-//       {
-//         internalType: 'OrderId',
-//         name: 'id',
-//         type: 'uint256',
-//       },
-//     ],
-//     name: 'decode',
-//     outputs: [
-//       {
-//         internalType: 'BookId',
-//         name: 'bookId',
-//         type: 'uint192',
-//       },
-//       {
-//         internalType: 'Tick',
-//         name: 'tick',
-//         type: 'int24',
-//       },
-//       {
-//         internalType: 'uint40',
-//         name: 'index',
-//         type: 'uint40',
-//       },
-//     ],
-//     stateMutability: 'pure',
-//     type: 'function',
-//   },
-// ] as const
-//
-// const ORDER_ID_WRAPPER_ADDRESS = '0xE6F891AB0cEE5b3Cc2fc1888D7A244eCc5bC1129'
-//
-// const publicClient = createPublicClient({
-//   chain: arbitrumSepolia,
-//   transport: http(FORK_URL),
-// })
+import { expect, test } from 'vitest'
 
-// TODO: Uncomment this test when the order id wrapper is deployed on the testnet
-// test('check fromOrderId function', async () => {
-//   const openOrders = await getOpenOrders({
-//     chainId: cloberTestChain.id,
-//     userAddress: '0x000000000000000000000000000000000000dead',
-//   })
-//   for (const openOrder of openOrders) {
-//     const result = await publicClient.readContract({
-//       address: ORDER_ID_WRAPPER_ADDRESS,
-//       abi: _abi,
-//       functionName: 'decode',
-//       args: [BigInt(openOrder.id)],
-//     })
-//     const { bookId, tick, index } = fromOrderId(BigInt(openOrder.id))
-//     expect(result[0]).toEqual(bookId)
-//     expect(result[1]).toEqual(Number(tick))
-//     expect(result[2]).toEqual(Number(index))
-//   }
-// })
+import { fromOrderId } from '../src/entities/open-order/utils/order-id'
+
+test('check fromOrderId function - 1', async () => {
+  const { bookId, tick, index } =
+    fromOrderId(
+      53566444723624360785422944131996696091141564570442382186621897550883387867403n,
+    )
+  expect(bookId).toEqual(
+    '2903842787083910905150096686205997338709207897290567260368',
+  )
+  expect(tick).toEqual(-259218)
+  expect(index).toEqual(267)
+})
+
+test('check fromOrderId function - 2', async () => {
+  const { bookId, tick, index } =
+    fromOrderId(
+      53566444723624360785422944131996696091141564570442382186622182562989005078795n,
+    )
+  expect(bookId).toEqual(
+    '2903842787083910905150096686205997338709207897290567260368',
+  )
+  expect(tick).toEqual(-1)
+  expect(index).toEqual(267)
+})
