c88071f540abc7f9cff1057d04cc1b03a9c07ebe
graykode <nlkey2022@gmail.com>
2025-05-15 10:48:13 +0900

    feat: update Rise Sepolia subgraph URL to use latest endpoint

--- src/constants/chain-configs/subgraph.ts
+++ src/constants/chain-configs/subgraph.ts
@@ -16,7 +16,7 @@ export const SUBGRAPH_URL: {
   // [CHAIN_IDS.BERACHAIN_MAINNET]:
   //   'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-core-subgraph-berachain-mainnet/api',
   [CHAIN_IDS.RISE_SEPOLIA]:
-    'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/v2-subgraph-rise-sepolia/1443122/gn',
+    'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/v2-subgraph-rise-sepolia/latest/gn',
   [CHAIN_IDS.MONAD_TESTNET]:
     'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-subgraph-monad-testnet/api',
   // [CHAIN_IDS.SONIC_MAINNET]:
