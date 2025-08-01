203adfc7bcb636bed052efd7263d555ad123f94b
graykode <nlkey2022@gmail.com>
2025-08-01 13:45:58 +0900

    feat: add getVersion function to retrieve version from package.json

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ export * from './types'
 export * from './views'
 export * from './calls'
 export * from './utils'
+
+export const getVersion = () => {
+  const fs = require('fs')
+  const path = require('path')
+  const packageJsonPath = path.join(__dirname, '../package.json')
+  return JSON.parse(fs.readFileSync(packageJsonPath, 'utf8')).version as string
+}
