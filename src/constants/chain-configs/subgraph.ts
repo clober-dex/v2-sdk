a330b17013f18096c7d33b2f0360f9db35e2ef1e
graykode <nlkey2022@gmail.com>
2025-12-02 16:28:17 +0900

    fix: update Monad Mainnet subgraph URL to the new endpoint

--- src/constants/chain-configs/subgraph.ts
+++ src/constants/chain-configs/subgraph.ts
@@ -20,7 +20,7 @@ export const SUBGRAPH_URL: {
   [CHAIN_IDS.MONAD_TESTNET]:
     'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/v2-subgraph-monad-testnet/latest/gn',
   [CHAIN_IDS.MONAD_MAINNET]:
-    'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/v2-subgraph-monad/latest/gn',
+    'https://api.subgraph.ormilabs.com/api/public/27ad58eb-e12c-4b7b-8df8-0560d8e26b37/subgraphs/v2-subgraph-monad/latest/gn',
   // [CHAIN_IDS.SONIC_MAINNET]:
   //   'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/v2-core-subgraph-sonic-mainnet/api',
 }
