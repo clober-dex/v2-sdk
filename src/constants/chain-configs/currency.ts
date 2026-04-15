6bd6365eeb252883a714f2d4fb0e442feda6ca9c
graykode <nlkey2022@gmail.com>
2026-04-15 14:37:19 +0900

    feat: update symbol for USD Coin (Arb1) to USDC.e in currency.ts

--- src/constants/chain-configs/currency.ts
+++ src/constants/chain-configs/currency.ts
@@ -161,7 +161,7 @@ export const STABLE_COINS: {
     {
       address: getAddress('0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'),
       name: 'USD Coin (Arb1)',
-      symbol: 'USDbC',
+      symbol: 'USDC.e',
       decimals: 6,
     },
   ],
